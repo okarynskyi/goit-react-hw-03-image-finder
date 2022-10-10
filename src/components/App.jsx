08901e91ee62e7c499f6a4a6f9609c82359085ff
Oleh Okarynskyi <okarynskyy@gmail.com>
2022-10-10 21:31:36 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
-import { Component } from "react";
+// import { Component } from "react";
 // import css from './App.module.css';
 
-export class App extends Component {
-  state = {}
+// export class App extends Component {
+//   state = {}
 
-  render() {}
-}
+//   render() {}
+// }
